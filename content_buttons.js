6cb6f1d004e8ab066d22ccd497f8c4f8adf39670
NINETEENTEN123 <nineteenten123@gmail.com>
2025-07-10 22:23:00 -0400

    Create content_buttons.js

---
+++ content_buttons.js
@@ -0,0 +1,6 @@
+function showTutorials(){
+  document.getElementById("tutorials").style.display="block";
+}
+function hideTutorials(){
+  document.getElementById("tutorials").style.display="none";
+}
